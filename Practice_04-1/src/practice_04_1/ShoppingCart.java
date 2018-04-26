9b9beac8fd65c90009501524f860ef753252ac2f
RotaruLilia <lilia.rotaru@calc.utm.md>
2018-04-26 19:36:14 +0300

    leson3

---
+++ Practice_04-1/src/practice_04_1/ShoppingCart.java
@@ -0,0 +1,18 @@
+/* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+
+package practice_04_1;
+
+public class ShoppingCart {
+    public static void main(String[] args){
+        // Declare and initialize String variables.  Do not initialize message yet.
+        
+        // Assign the message variable 
+        
+        // Print and run the code
+        String custName = "Many Smith wants to puchase", itemDisc = "a Shirt" ;
+        String message;
+        message = custName + itemDisc;
+        System.out.println (message);
+
+    }
+}
